d2a984ec47d55f5ddc0bf467befe11ce1153fd58
José Lazaretti <eduardo1793@gmail.com>
2026-01-10 14:41:11 -0300

    Create main page

---
+++ app/components/trend.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="font-bold" :class="[color]">{{ title }}</div>
+    <div class="text-2xl font-extrabold text-black dark:text-white mb-2">
+      <USkeleton class="h-8 w-full" v-if="loading" />
+      <div v-else>${{ amount }}</div>
+    </div>
+    <div>
+      <USkeleton class="h-6 w-full" v-if="loading" />
+      <div v-else class="flex gap-1 items-center text-sm">
+        <UIcon name="i-heroicons-arrow-trending-up" class="w-6 h-6" :class="[color]" />
+        <div class="text-gray-500 dark:text-gray-400">30% vs last period</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  lastAmount: {
+    type: Number,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style scoped>
+@reference "../assets/css/main.css";
+.green {
+  @apply text-green-600 dark:text-green-400;
+}
+.red {
+  @apply text-red-600 dark:text-red-400;
+}
+.blue {
+  @apply text-blue-600 dark:text-blue-400;
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <section class="flex items-center justify-between mb-10">
+      <h1 class="text-4xl font-extrabold">Summary</h1>
+      <USelectMenu
+        v-model="viewSelected"
+        :items="transactionViewOptions"
+        class="w-48"
+        button-class="bg-white border border-gray-300 text-gray-700"
+      />
+    </section>
+    <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
+      <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
+      <Trend color="red" title="Expenses" :amount="2000" :last-amount="1500" :loading="false" />
+      <Trend color="blue" title="Savings" :amount="1500" :last-amount="1200" :loading="false" />
+      <Trend
+        color="green"
+        title="Investments"
+        :amount="3500"
+        :last-amount="2800"
+        :loading="false"
+      />
+    </section>
+  </div>
+</template>
+
+<script setup>
+import { transactionViewOptions } from '~/constants.ts';
+const viewSelected = ref(transactionViewOptions[1]);
+</script>
