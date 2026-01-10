39153c3d953b2ef8f56505be5296a2516675383e
José Lazaretti <eduardo1793@gmail.com>
2026-01-10 14:40:54 -0300

    Create header

---
+++ app/components/app-header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="flex justify-between items-center mt-10">
+    <NuxtLink to="/" class="text-xl font-bold"> Finance Tracker </NuxtLink>
+
+    <div>
+      <UAvatar size="sm" src="https://avatars.githubusercontent.com/u/11966764?v=4" alt="Avatar" />
+    </div>
+  </header>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container mx-auto max-w-4xl">
+    <AppHeader />
+    <main class="my-10">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Inter&display=swap',
+      crossorigin: '',
+    },
+  ],
+});
+</script>
+
+<style>
+@reference "../assets/css/main.css";
+
+body {
+  font-family: 'Inter';
+}
+body {
+  @apply dark:bg-gray-900 bg-white;
+}
+</style>
