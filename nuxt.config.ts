7675a41264b2637162e1e1d62373c65dbf74654b
José Lazaretti <eduardo1793@gmail.com>
2026-01-10 13:51:59 -0300

    Add NuxtUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
 });
