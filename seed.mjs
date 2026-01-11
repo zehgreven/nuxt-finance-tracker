bc160e2a0d64b1c9538c6af4d4cdd2b5193dd9f0
José Lazaretti <eduardo1793@gmail.com>
2026-01-11 11:53:51 -0300

    Update seed

--- seed.mjs
+++ seed.mjs
@@ -18,50 +18,46 @@ async function seedTransactions() {
 
   let transactions = [];
 
-  for (let year = new Date().getFullYear(); year > new Date().getFullYear() - 2; year--) {
-    for (let i = 0; i < 10; i++) {
-      const date = new Date(
-        year,
-        faker.number.int({ min: 0, max: 11 }),
-        faker.number.int({ min: 1, max: 28 }),
-      );
+  for (let i = 0; i < 20; i++) {
+    const date = new Date(
+      faker.date.between({ from: `2024-01-01T00:00:00.000Z`, to: new Date().toISOString() }),
+    );
 
-      let type, category;
-      const typeBias = Math.random();
+    let type, category;
+    const typeBias = Math.random();
 
-      if (typeBias < 0.85) {
-        type = 'Expense';
-        category = faker.helpers.arrayElement(categories); // Category only for 'Expense'
-      } else if (typeBias < 0.95) {
-        type = 'Income';
-      } else {
-        type = faker.helpers.arrayElement(['Saving', 'Investment']);
-      }
-
-      let amount;
-      switch (type) {
-        case 'Income':
-          amount = faker.number.int({ min: 2000, max: 5000 });
-          break;
-        case 'Expense':
-          amount = faker.number.int({ min: 100, max: 1000 });
-          break;
-        case 'Saving':
-        case 'Investment':
-          amount = faker.number.int({ min: 5000, max: 10000 });
-          break;
-        default:
-          amount = 0;
-      }
-
-      transactions.push({
-        created_at: date,
-        amount,
-        type,
-        description: faker.lorem.sentence(),
-        category: type === 'Expense' ? category : null, // Category only for 'Expense'
-      });
+    if (typeBias < 0.85) {
+      type = 'Expense';
+      category = faker.helpers.arrayElement(categories); // Category only for 'Expense'
+    } else if (typeBias < 0.95) {
+      type = 'Income';
+    } else {
+      type = faker.helpers.arrayElement(['Saving', 'Investment']);
     }
+
+    let amount;
+    switch (type) {
+      case 'Income':
+        amount = faker.number.int({ min: 2000, max: 5000 });
+        break;
+      case 'Expense':
+        amount = faker.number.int({ min: 100, max: 1000 });
+        break;
+      case 'Saving':
+      case 'Investment':
+        amount = faker.number.int({ min: 5000, max: 10000 });
+        break;
+      default:
+        amount = 0;
+    }
+
+    transactions.push({
+      created_at: date,
+      amount,
+      type,
+      description: faker.lorem.sentence(),
+      category: type === 'Expense' ? category : null, // Category only for 'Expense'
+    });
   }
 
   const { error: insertError } = await supabase.from('transactions').upsert(transactions);
